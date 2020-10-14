8d5e9584771493c86629cc5568131ac8974faf09
oh <oh@spacemetric.com>
2020-10-14 09:18:33 +0200

    - Initial content

---
+++ src/main/java/com/github/oscarhaglund/jpms/javadoc/linux/Foo.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (C) Spacemetric AB, Sweden
+ * http://www.spacemetric.com/
+ */
+package com.github.oscarhaglund.jpms.javadoc.linux;
+
+
+/**
+ * Test class for seeing if javadoc can be generated in JMPS setting on linux
+ * @author Oscar Haglund
+ *
+ */
+public class Foo {
+	private String foo;
+	private String bar;
+	
+	/**
+	 * @return the foo
+	 */
+	public String getFoo() {
+		return foo;
+	}
+	
+	/**
+	 * @param foo the foo to set
+	 */
+	public void setFoo(String foo) {
+		this.foo = foo;
+	}
+	
+	/**
+	 * @return the bar
+	 */
+	public String getBar() {
+		return bar;
+	}
+	
+	/**
+	 * @param bar the bar to set
+	 */
+	public void setBar(String bar) {
+		this.bar = bar;
+	}
+	
+	
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+/**
+ * @author Oscar Haglund
+ *
+ */
+module com.github.oscarhaglund.jpms.javadoc.linux {
+	exports com.github.oscarhaglund.jpms.javadoc.linux;
+}
